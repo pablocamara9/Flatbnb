46297020e8175ba1daaf620d7fe4d3c9c62c37e6
Pablo <camara.gapab23@triana.salesianos.edu>
2025-02-20 12:13:37 +0100

    Agregado proyecto

---
+++ flatbnb/src/main/java/com/salesianostriana/flatbnb/FlatbnbApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.flatbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatbnbApplication.class, args);
+	}
+
+}
---
+++ flatbnb/src/test/java/com/salesianostriana/flatbnb/FlatbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.flatbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

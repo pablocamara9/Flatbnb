3ea70692791fc193304f82c5e8832015d76e9fa1
Pablo <camara.gapab23@triana.salesianos.edu>
2025-02-20 14:21:40 +0100

    Avances en seguridad, temrinar SecurityConfig en casa.

--- flatbnb/src/main/java/com/salesianostriana/flatbnb/security/SecurityConfig.java
+++ flatbnb/src/main/java/com/salesianostriana/flatbnb/security/SecurityConfig.java
@@ -1,4 +1,62 @@
 package com.salesianostriana.flatbnb.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
 public class SecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        AuthenticationManager authenticationManager =
+                authenticationManagerBuilder.authenticationProvider(authenticationProvider())
+                        .build();
+
+        return authenticationManager;
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider p = new DaoAuthenticationProvider();
+
+        p.setUserDetailsService(userDetailsService);
+        p.setPasswordEncoder(passwordEncoder);
+
+        return p;
+    }
+/*
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable());
+        http.cors(Customizer.withDefaults());
+        http.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.exceptionHandling(excepz -> excepz
+                .authenticationEntryPoint(authenticationEntrypoint)
+                .accessDeniedHandler(new JwtAccessDeniedHandler())
+        );
+
+    }
+*/
 }

c42dfd0102eadef592cc6ca89a2e87630572a274
Pablo <camara.gapab23@triana.salesianos.edu>
2025-02-25 11:49:14 +0100

    Agregado findById en controlador y servicio de propietario.

--- flatbnb/src/main/java/com/salesianostriana/flatbnb/controller/PropietarioController.java
+++ flatbnb/src/main/java/com/salesianostriana/flatbnb/controller/PropietarioController.java
@@ -1,15 +1,17 @@
 package com.salesianostriana.flatbnb.controller;
 
 import com.salesianostriana.flatbnb.dto.piso.GetAllPropietariosDto;
+import com.salesianostriana.flatbnb.dto.propietario.GetPropietarioDto;
 import com.salesianostriana.flatbnb.service.PropietarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +24,11 @@ public class PropietarioController {
     public ResponseEntity<GetAllPropietariosDto> findAll() {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(GetAllPropietariosDto.fromDto(propietarioService.findAll()));
+    }
+
+    @GetMapping("{id}")
+    public GetPropietarioDto findById(@PathVariable UUID id) {
+        return GetPropietarioDto.of(propietarioService.findById(id));
     }
 
 }

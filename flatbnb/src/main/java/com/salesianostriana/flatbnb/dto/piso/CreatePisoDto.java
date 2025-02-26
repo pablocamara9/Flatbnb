93198f6787374ef0127b1749cd4a90002a8665cd
Pablo <camara.gapab23@triana.salesianos.edu>
2025-02-26 14:01:36 +0100

    Agregada validación para crear y editar pisos.

--- flatbnb/src/main/java/com/salesianostriana/flatbnb/dto/piso/CreatePisoDto.java
+++ flatbnb/src/main/java/com/salesianostriana/flatbnb/dto/piso/CreatePisoDto.java
@@ -1,12 +1,17 @@
 package com.salesianostriana.flatbnb.dto.piso;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
 public record CreatePisoDto(
+        @NotBlank(message = "La dirección no puede estar vacía")
         String direccion,
         double metrosCuadrados,
         int numHabitaciones,
         String observaciones,
+        @NotNull(message = "El id del propietario no puede estar vacío")
         UUID idPropietario
 ) {
 }

227afbba51fbd8933b16d60b1e7d189fbfd3756e
Pablo <camara.gapab23@triana.salesianos.edu>
2025-06-16 10:58:14 +0200

    Datos de main-page paginados

--- flatbnb-angular/src/app/components/main-page/main-page.component.ts
+++ flatbnb-angular/src/app/components/main-page/main-page.component.ts
@@ -11,27 +11,41 @@ import { Anuncio, Anuncios } from '../../models/anuncio.model';
 })
 export class MainPageComponent implements OnInit {
   anuncios: Anuncio[] = [];
+  paginaActual: number = 0;
+  totalPaginas: number = 1;
 
   constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
-    this.http.get<any>('http://localhost:8080/anuncio/')
+    this.cargarAnuncios(0);
+  }
+
+  cargarAnuncios(pagina: number) {
+    this.http.get<any>(`http://localhost:8080/anuncio/?page=${pagina}`)
       .subscribe({
         next: (data) => {
-          console.log(data);
-          
           if (data && data.content) {
             this.anuncios = data.content;
-            console.log(this.anuncios);
-            
+            this.paginaActual = data.number;
+            this.totalPaginas = data.totalPages;
           } else {
             this.anuncios = [];
+            this.paginaActual = 0;
+            this.totalPaginas = 1;
           }
         },
         error: (err) => {
           this.anuncios = [];
+          this.paginaActual = 0;
+          this.totalPaginas = 1;
         }
       });
+  }
+
+  cambiarPagina(nuevaPagina: number) {
+    if (nuevaPagina >= 0 && nuevaPagina < this.totalPaginas) {
+      this.cargarAnuncios(nuevaPagina);
+    }
   }
 
   verDetalle(id: string) {
